2b14c94cdecaac9815b9b882e2d7419899e0d39a
João Gonçalves <joaopedro.jp.4998@gmail.com>
2022-11-29 17:29:28 +0000

    Add controls features

---
+++ script.js
@@ -0,0 +1,83 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const audio = document.querySelector("audio");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army(Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Jacinto Design",
+  },
+];
+
+let isPlaying = false;
+
+//Play
+const playSong = () => {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  audio.play();
+};
+
+//Pause
+const pauseSong = () => {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  audio.pause();
+};
+
+// Play or Pause Event Listener
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+//Update DOM
+const loadSong = (song) => {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  audio.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+};
+
+let songIndex = 0;
+loadSong(songs[songIndex]);
+
+const prevSong = () => {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+const nextSong = () => {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+// Event Listenet from prev and next buttons
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
